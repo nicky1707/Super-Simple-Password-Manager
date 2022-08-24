1eb1cf8135be4b62664081efa7bc3289eeda8bdb
nicky1707 <1901711039027@mcc.edu.in>
2022-08-24 20:03:22 +0530

    Updated code structure

--- main.py
+++ main.py
@@ -1,109 +1,124 @@
-import sqlite3
-import random
 import clipboard
-import secrets
-import database as d
-from os import path
+import sqlite3
+import os
+from pathlib import Path
 
+# Create a new database at appdata if it doesn't exist
+path = os.getenv('APPDATA')
+try:
+    os.mkdir(path + '\\test')
+except FileExistsError:
+    pass
 
-# random password generator
-def pas_gen():
-    lower = 'abcdefghijklmnopqrstuvwxyz'
-    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    nums = str(secrets.randbits(35))
-    symbols = '!@#$%^&*'
-    string = lower + upper + nums + symbols
-    stage_1 = ''.join(random.sample(string,15))
-    stage_2 = ''.join(random.sample(stage_1,15))
-    return stage_2
-# master password creator
-def master_pass_creator():
-	if path.exists("passwords.db"):
-		
+fle = Path(path + '\\test\\pass.db')
+fle.touch(exist_ok=True)
 
-		pass
-	else:
-		master = input('create a master pass: ')
-		d.store_master(password=master)
+# Class based sqlite3 database for storing passwords 
+class Database:
+    def __init__(self):
+        self.con = sqlite3.Connection(path+'\\test\\pass.db')
+        self.cur = self.con.cursor()
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS user_pass
+            (name text PRIMARYKEY, pass text,description text)
+        """)
+        self.con.commit()
 
+    def check(self):
+        print('Hello world')
 
-# main function
+    def get_pass(self, name):
+        self.cur.execute("""
+            SELECT pass FROM user_pass WHERE name = ?
+        """, (name,))
+        return self.cur.fetchone()
+
+    def add_pass(self, name, passw, description=""):
+        self.cur.execute("""
+            INSERT OR IGNORE INTO user_pass values(?,?,?)""", (name, passw, description))
+        self.con.commit()
+
+    def delete_pass(self, name):
+        self.cur.execute("""
+            DELETE FROM user_pass WHERE name = ?
+        """, (name,))
+        self.con.commit()
+
+    def update_pass(self, name, passw):
+        self.cur.execute("""
+            UPDATE user_pass SET pass = ? WHERE name = ?
+        """, (passw, name))
+        self.con.commit()
+
+    def get_all_pass(self):
+        self.cur.execute("""
+            SELECT * FROM user_pass
+        """)
+        return self.cur.fetchall()
+
+    def close(self):
+        self.con.close()
+
+# Create a new database object
+new_databse = Database()
+
+# Main function loop
 def main():
 
-	master_pass_creator()
-
-	# looping the menu
-	loop = True
-
-	while loop == True:
-
-		print("""
-		1.Create new password
-		2.Saved passwords
-		3.Delete a password
-		4.Quit
-			""")
-		print('-'*35)
-
-		user_input = input('Enter Query: ')
-
-
-		# Creating a db, password, copy to clipboard
-		if user_input == '1':
-			data = pas_gen()
-			print('-'*35)
-			user_key = input('Enter a key to store: ')
-			print('-'*35)
-			clipboard.copy(data)
-			
-			password = user_key, data   # packing key, password into a tuple
-			d.store_pass(password)
-
-
-		# showing all keys from db to query, copy to clipboard
-		elif user_input == '2':
-
-			master_db = d.query_master('mas')
-			master_auth = input('Enter master pass: ')
-
-			if master_auth == master_db[2]:
-				queried_passwords = d.query_all()
+    loop = True
+    while loop == True:
+        print("""
+  ________________________
+ | 1.Create new password  |
+ | 2.Saved passwords      |
+ | 3.Delete a password    |
+ | 4.Quit                 |
+ |________________________|
  
-				for i in queried_passwords:    
-					print(i[1])
-				print('-'*35)				
+			""")
+        choice = input(":: ")
 
-				user_key_choice = input('Enter key to copy: ')
-				pass_out = d.query_pass(user_key_choice)
-				clipboard.copy(pass_out[2])
-				print('copied to clipboard..')
+        if choice == "1":
+            name = input("Name: ")
+            password = input("Password: ")
+            description = input("Description: ")
+            new_databse.add_pass(name, password, description)
+            clipboard.copy(password)
+            print("Password copied to clipboard")
 
-			else:
-				print('Invalid password')
+        elif choice == "2":
+            query = new_databse.get_all_pass()
+            print("")
+
+            for i in query:
+                print(f" {i[0]} \t  : {i[1]} {i[2]} ")
+        elif choice == "3":
+            query = new_databse.get_all_pass()
+            print('')
+            for i in query:
+                print(f" {i[0]} \t  : {i[1]} {i[2]} ")
+            print('')
+            name = input("Delete Name :: ")
+            conformation = input(
+                "Are you sure you want to delete this password? [ y/n ]: ")
+
+            if conformation == "y":
+                new_databse.delete_pass(name)
+                print("Password deleted")
+
+            else:
+                print("Password not deleted")
+
+        elif choice == "4":
+            loop = False
+            print("Goodbye !!")
+
+        else:
+            print(choice)
+            password = new_databse.get_pass(choice)[0]
+            print("Password copied to clipboard !")
+            clipboard.copy(password)
 
 
-
-		# showing all keys from db to query, delete a key&pass from db
-		elif user_input == '3':
-
-			queried_passwords = d.query_all()
-
-			for i in queried_passwords:
-				print(i[1])
-			print('-'*35)
-
-			user_delete = input('Enter key to delete: ')
-			d.delete_pass(user_delete)
-			print('-'*35)
-			print('Entry Deleted')
-
-
-		# exiting the menu loop if input  = quit
-		elif user_input == '4':
-			print('Quiting.....')
-			loop = False
-
-
-# calling the main function			
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
